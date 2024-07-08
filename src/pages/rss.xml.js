defbe7846649f07272ebbb257555790e2ee3d9c7
Jan-Vil <jvilaplan@ies-eugeni.cat>
2024-07-08 15:51:37 +0200

    Astro tutorial Part 6: Astro islands

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -5,8 +5,9 @@ export async function GET(context) {
     title: 'Astro Learner | Blog',
     description: 'My journey learning Astro',
     site: context.site,
-    items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
-    customData: `<language>en-us</language>`,
+    items: await pagesGlobToRssItems(import.meta.glob
+      ('./**/*.md')),
+    customData: `<language>es-us</language>`,
   });
 }
 
